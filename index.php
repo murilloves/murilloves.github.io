1a503b036c00c5cda8242e1a8580df4d758d1516
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-03-04 16:37:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link href = "assets/styles.css" rel = "stylesheet">
+	</head>
+
+	<body id = "MiniContosAmbulantes">
+		<div class = "wrapper">
+			<div id = "banner">
+				<a href ="/" title = "stylesheet">
+					<img src ="assets/img/banner.jpg" alt = "MiniContosAmbulantes">
+				</a>
+			</div><!-- banner -->
+			<div id = "nav">
+				
+				
+			</div><!-- nav -->
+
+			<div class = "content">
+				<div id = "footer" class = "cf">
+					<div class = "column three">
+						<strong>Phone</strong>
+						+55(62)985.545.576
+					</div><!-- column 1 -->
+
+					<div class = "column three">
+						<strong>Location</strong>
+						123 Av. Boulevard<br>
+						Goiânia, GO
+					</div><!-- column 2 -->
+
+					<div class = "column three last">
+						<strong>Hours</strong>
+
+						<em>Monday - Friday</em>
+						9:00am - 5:00pm<br><br>
+
+						<em>Saturday - Sunday</em>
+						Closed<br><br>
+					</div><!-- column 3 -->
+
+				</div><!-- footer -->
+				<small>&copy;2016 - MiniContosAmbulantes</small>
+			</div><!-- content -->
+		</div><!-- wrapper -->
+		<div class = "copyright-info">
+			<?php include('assets/includes/copyright.php') ?>
+		</div><!-- copyright-info -->
+
+	</body>
+
+</html>
