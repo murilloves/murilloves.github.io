3101b75a0d72e2553b23b64e2b5d4e70aa66745f
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-03-04 16:49:34 -0300

    Create copyright.php

---
+++ assets/includes/copyright.php
@@ -0,0 +1,2 @@
+<h4>MVeS - WebPages</h4>
+<h5>Copyright &copy;<?php echo date('Y');?> <a href="https://github.com/murilloves" target="_blank">Murillo Vieira e Silva</a> of <a href="http://inf.ufg.br/" target="_blank">INF-UFG</a></h5>
