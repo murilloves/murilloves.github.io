329b8bb54c152195fe7d634d442b4ab370acf920
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-03-04 19:00:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,45 +6,58 @@
 	</head>
 
 	<body id = "MiniContosAmbulantes">
+
 		<div class = "wrapper">
-			<div id = "banner">
-				<a href ="/" title = "stylesheet">
-					<img src ="assets/img/banner.jpg" alt = "MiniContosAmbulantes">
+
+			<div id="banner">
+				<a href="/" title="stylesheet">
+					<img src="assets/img/banner.jpg" alt="MiniContosAmbulantes">
 				</a>
 			</div><!-- banner -->
-			<div id = "nav">
-				
-				
-			</div><!-- nav -->
 
-			<div class = "content">
-				<div id = "footer" class = "cf">
-					<div class = "column three">
-						<strong>Phone</strong>
+			<div id="nav">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="contos.php">Contos</a></li>
+					<li><a href="equipe.php">Equipe</a></li>
+					<li><a href="contato.php">Contato</a></li>
+				</ul>
+			</div><!-- nav -->
+			
+			<div class="content">
+
+				<div id="footer" class="cf">
+
+					<div class="column three">
+						<strong>Telefone</strong>
 						+55(62)985.545.576
 					</div><!-- column 1 -->
 
-					<div class = "column three">
-						<strong>Location</strong>
+					<div class="column three">
+						<strong>Localização</strong>
 						123 Av. Boulevard<br>
 						Goiânia, GO
 					</div><!-- column 2 -->
 
-					<div class = "column three last">
-						<strong>Hours</strong>
+					<div class="column three last">
+						<strong>Horário</strong>
 
-						<em>Monday - Friday</em>
+						<em>Segunda - Sexta</em><br>
 						9:00am - 5:00pm<br><br>
 
-						<em>Saturday - Sunday</em>
-						Closed<br><br>
+						<em>Sábado - Domingo</em><br>
+						Fechado<br><br>
 					</div><!-- column 3 -->
 
 				</div><!-- footer -->
+
 				<small>&copy;2016 - MiniContosAmbulantes</small>
+
 			</div><!-- content -->
+
 		</div><!-- wrapper -->
-		<div class = "copyright-info">
+
+		<div class="copyright-info">
 			<?php include('assets/includes/copyright.php') ?>
 		</div><!-- copyright-info -->
 
