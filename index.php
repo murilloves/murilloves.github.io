0caa8ac3903b136c91699a9cd11346a00ed03e0a
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-03-08 14:51:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,66 +1,30 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title></title>
-		<link href = "assets/styles.css" rel = "stylesheet">
-	</head>
+<?php
+	define ("TITLE","MiniContosAmbulantes");
+	include ('includes/header.php');
+?>
 
-	<body id = "MiniContosAmbulantes">
+<style type="text/css">
+@font-face {
+    font-family: "Corona Typewriter";
+    src: url(fonts/corona3Typewriter.ttf) format("truetype");
+}
+</style>
+<font face="Corona Typewriter" >
 
-		<div class = "wrapper">
+<div>
+	<hr>
+	<h5><center>
+		Apesar de “Mini”, os contos são completos e a complexidade de sua pequenez os torna únicos e deliciosos. Sirva-se à vontade.
+	</center></h5>
+	<hr>
+</div>
 
-			<div id="banner">
-				<a href="/" title="stylesheet">
-					<img src="assets/img/banner.jpg" alt="MiniContosAmbulantes">
-				</a>
-			</div><!-- banner -->
 
-			<div id="nav">
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="contos.php">Contos</a></li>
-					<li><a href="equipe.php">Equipe</a></li>
-					<li><a href="contato.php">Contato</a></li>
-				</ul>
-			</div><!-- nav -->
-			
-			<div class="content">
+<font face="Helvetica" >
+<small>&copy;<?php echo date('Y')." - ".$siteName ?></small>
+	</div><!-- content -->
+</div><!-- wrapper -->
 
-				<div id="footer" class="cf">
-
-					<div class="column three">
-						<strong>Telefone</strong>
-						+55(62)985.545.576
-					</div><!-- column 1 -->
-
-					<div class="column three">
-						<strong>Localização</strong>
-						123 Av. Boulevard<br>
-						Goiânia, GO
-					</div><!-- column 2 -->
-
-					<div class="column three last">
-						<strong>Horário</strong>
-
-						<em>Segunda - Sexta</em><br>
-						9:00am - 5:00pm<br><br>
-
-						<em>Sábado - Domingo</em><br>
-						Fechado<br><br>
-					</div><!-- column 3 -->
-
-				</div><!-- footer -->
-
-				<small>&copy;2016 - MiniContosAmbulantes</small>
-
-			</div><!-- content -->
-
-		</div><!-- wrapper -->
-
-		<div class="copyright-info">
-			<?php include('assets/includes/copyright.php') ?>
-		</div><!-- copyright-info -->
-
-	</body>
-
-</html>
+<div class="copyright-info">
+	<?php include('assets/includes/copyright.php') ?>
+</div><!-- copyright-info -->
