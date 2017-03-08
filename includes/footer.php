3cbf32447c65c6e77200d93207b6fb4c604f6a99
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-03-08 14:54:49 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+			<div class="content">
+				<div id="footer" class="cf">
+
+					<div class="column three">
+						<strong>Telefone</strong>
+						+55(62)985.545.576
+					</div><!-- column 1 -->
+
+					<div class="column three">
+						<strong>Localização</strong>
+						123 Av. Boulevard<br>
+						Goiânia, GO
+					</div><!-- column 2 -->
+
+					<div class="column three last">
+						<strong>Horário</strong>
+
+						<em>Segunda - Sexta</em><br>
+						9:00am - 5:00pm<br><br>
+
+						<em>Sábado - Domingo</em><br>
+						Fechado<br><br>
+					</div><!-- column 3 -->
+
+				</div><!-- footer -->
+
+				<small>&copy;<?php echo date('Y')." - ".$siteName ?></small>
+
+			</div><!-- content -->
+
+		</div><!-- wrapper -->
+
+		<div class="copyright-info">
+			<?php include('assets/includes/copyright.php') ?>
+		</div><!-- copyright-info -->
+
+	</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php
+	$siteName = MiniContosAmbulantes;
+	include('includes/arrays.php');
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo TITLE ?></title>
+		<link href = "assets/styles.css" rel = "stylesheet">
+	</head>
+
+	<body id = "MiniContosAmbulantes">
+
+		<div class = "wrapper">
+
+			<div id="banner">
+				<a href="" title="stylesheet">
+					<img src="assets/img/banner.jpg" alt="MiniContosAmbulantes">
+				</a>
+			</div><!-- banner -->
+
+			<div id="nav">
+				<?php include('includes/navigation.php'); ?>
+			</div><!-- nav -->
