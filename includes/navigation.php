53acbfd189ef8997a49ebe8a96d17b0aa505f022
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2017-03-09 00:58:02 -0300

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,5 +1,15 @@
-<ul>
+<style type="text/css">
+	@font-face {
+	    font-family: "Brain Flower";
+	    src: url(fonts/brainFlowerEuro.ttf) format("truetype");
+	}
+</style>
+<font face="Brain Flower" >
+
+<ul><h4>
 	<li><a href="index.php">Home</a></li>
 	<li><a href="contos.php">Contos</a></li>
 	<li><a href="contato.php">Contato</a></li>
 </ul>
+
+<font face="Helvetica Neue", Helvetica, Arial, sans-serif>
