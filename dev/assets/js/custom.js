7d1fe88a411e8114c7d5dd2ce5aaf617580200ed
murilloves <murillo.vieiraesilva@gmail.com>
2018-08-20 00:30:08 -0300

    Improvements on Website

---
+++ dev/assets/js/custom.js
@@ -0,0 +1,25 @@
+function copyToClipboard(element) {
+    /* Get the text to be copied */
+    var str = element.innerHTML;
+
+    /* Display the input element */
+    document.getElementById("inputCopy").style.display = 'block';
+
+    /* Put the text inside input */
+    document.getElementById("inputCopy").value = str;
+
+    /* Get the text field */
+    var copyTextInput = document.getElementById("inputCopy");
+
+    /* Select the text field */
+    copyTextInput.select();
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Hide the input element */
+    document.getElementById("inputCopy").style.display = 'none';
+
+    /* Alert the copied text */
+    alert("Text copied to clipboard");
+}
