3f0ba175ade91bbf97ef37ab765b47fc782b9b33
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2018-11-23 18:18:48 -0200

    Add files via upload

---
+++ murilloVieira.js
@@ -0,0 +1,15 @@
+
+
+function murilloVieira(jobsArray, coffee) {
+  const hardWork = 'me' + coffee;
+  let money = jobsArray.array.forEach(job => {
+      job + hardWork;
+  });
+  return money;
+}
+
+function salary(money) {
+    let buyFood = money;
+    return buyFood;
+}
+
