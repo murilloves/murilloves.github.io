bb12de4e1aaa0fca0699807a46d9278c1bbc2609
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2019-06-27 01:50:30 -0300

    Add files via upload

---
+++ cv-clean/Career.svelte
@@ -0,0 +1,67 @@
+<div class="section-summary">
+    <h3 class="subtitle">
+        CAREER HISTORY
+    </h3>
+
+    <div>
+        <strong>Assistant at Sao Joao</strong>
+        <div class="tiny-desc">Largest eggs producer in south Brazil</div>
+        <div>Sept ‘09 – present</div>
+
+        <ul class="topic decimal">
+            <li>Insert content. Highlight responsibilities and key achievements/deliverables.</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+        </ul>
+    </div>
+    <div>
+        <strong>Assistant at Sao Joao</strong>
+        <div>Sept ‘09 – Sept '09 (2 years)</div>
+
+        <ul class="topic circle">
+            <li>Insert content. Highlight responsibilities and key achievements/deliverables.</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+        </ul>
+    </div>
+    <div>
+        <strong>Assistant at Sao Joao</strong>
+        <div>Aug ‘02 – Sept '06 (4 years)</div>
+
+        <ul class="topic upper-roman">
+            <li>Insert content. Highlight responsibilities and key achievements/deliverables.</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+            <li>Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+        </ul>
+    </div>
+</div>
+
+<style>
+	.subtitle {
+		text-transform: uppercase;
+		text-decoration: underline;
+	}
+	.tiny-desc {
+		font-size: 12px;
+		font-style: italic;
+	}
+	ul.circle > li {
+		list-style-type: circle;
+	}
+	ul.decimal > li {
+		list-style-type: decimal;
+	}
+	ul.roman > li {
+		list-style-type: lower-roman;
+	}
+	ul.upper-roman > li {
+		list-style-type: upper-roman;
+	}
+	li {word-wrap: break-word;}
+</style>
---
+++ cv-clean/Summary.svelte
@@ -0,0 +1,16 @@
+<div class="section-summary">
+    <h3 class="subtitle">
+        PERSONAL SUMMARY
+    </h3>
+    <div class="paragraph">
+        Your summary should be no more than 3/4 lines. It should highlight relevant experience, skills and achievements. For optimum benefit, tailor your summary for each job you are targeting. 
+    </div>
+</div>
+
+<style>
+	.subtitle {
+		text-transform: uppercase;
+		text-decoration: underline;
+	}
+	.paragraph {text-align: justify}
+</style>
