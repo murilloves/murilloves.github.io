5e88b6679b86c320af25f5cee3f31ade4a71f86f
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2019-06-27 01:49:56 -0300

    Update main.js

--- cv-clean/main.js
+++ cv-clean/main.js
@@ -1,4 +1,19 @@
 import App from './App.svelte';
+import Personal from './Personal.svelte';
+
+const personal = new Personal({
+	target: document.body,
+	props: {
+		name: 'Murillo Silva',
+		address: '09 H-40 Street - Santa Fe - Aparecida de Goiania',
+		email: 'murillov.dev@gmail.com',
+		phone: '+55 62 983-196-600',
+		linkedin: {
+			url: 'www.linkedin.com/in/murillo-vieira',
+			link: 'https://linkedin.com/in/murillo-vieira'
+		}
+	}
+});
 
 const app = new App({
 	target: document.body,
@@ -14,4 +29,4 @@ const app = new App({
 	}
 });
 
-export default app;
+export default { personal, app };
