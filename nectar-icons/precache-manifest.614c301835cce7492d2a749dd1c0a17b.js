f2ea6b2ff2f02230906d3a9e9faa956f745ea0a9
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2019-09-01 12:34:02 -0300

    Add files via upload

---
+++ nectar-icons/precache-manifest.614c301835cce7492d2a749dd1c0a17b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2267870d9cd2a65eef84fd1176297cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a57cc348cae4e7c67576",
+    "url": "/static/css/main.62397d4d.chunk.css"
+  },
+  {
+    "revision": "0b9dc07a984a9ee12bd3",
+    "url": "/static/js/2.fe746d07.chunk.js"
+  },
+  {
+    "revision": "a57cc348cae4e7c67576",
+    "url": "/static/js/main.43d05364.chunk.js"
+  },
+  {
+    "revision": "8c1db6b3d8288aa9a0b5",
+    "url": "/static/js/runtime~main.e24f384d.js"
+  }
+]);
